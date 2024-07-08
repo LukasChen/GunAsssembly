04ec173d71f10df610e8db7d861ab398da6ff896
LukasChen <villagelukaschen@gmail.com>
2024-07-08 11:30:16 +0800

    init

---
+++ Assets/Scripts/ShellEject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GunAssembly {
+    public class ShellEject : MonoBehaviour {
+        [SerializeField] private Transform _shellSpawn;
+
+        public void Eject() {
+            Debug.Log("Triggered");
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Rigidbody rb = cube.AddComponent<Rigidbody>();
+            cube.transform.position = _shellSpawn.position;
+            cube.transform.localScale = Vector3.one * 0.01f;
+            rb.velocity = new Vector3(0, 5f, 1f);
+            //rb.AddForce(transform.right * 20f, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Animations;
+using UnityEngine;
+
+namespace GunAssembly {
+    public class WeaponController : MonoBehaviour {
+        [SerializeField] private AnimatorController _newAnim;
+
+        private Animator _animator;
+
+        private void Start() {
+            _animator = GetComponent<Animator>();
+        }
+        
+        public void SwitchAnim() {
+            _animator.runtimeAnimatorController = _newAnim;
+        }
+    }
+}
